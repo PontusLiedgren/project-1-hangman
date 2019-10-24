f599d1ad1488fe1de5ddd90a19fdbe3824f9eb33
Pontus Liedgren <pop2strong4u@gmail.com>
2019-10-24 19:19:11 +0200

    early stages of the hangman game succesfully completed

--- hangman.py
+++ hangman.py
@@ -14,7 +14,7 @@ hangman_words = ['gubbe', 'by', 'tyskland', 'fönster', 'nationalencyclopedi', '
 secret_word = random.choice(hangman_words)  
 turns = 12
 guessed_letters = []
-
+correct_letters = []
 isPlaying = True
 
 # funktion för gissade bokstäver. 
@@ -32,20 +32,34 @@ while isPlaying:
     print_secret_word()
 
     guess = input("Guess a letter: ").lower()
-    guessed_letters.append(guess)
-
-    if guess in secret_word:
-        print("Korrekt! Du har " + str(turns) + " kvar")
-    else:
-        turns -= 1
-        print("Fel! Du har " + str(turns) + " kvar")
+    if len(guess) > 1: 
+        print("Ogiltig inmatning! \n")
+    elif len(guess) == 1:
+        if guess not in guessed_letters:
+            guessed_letters.append(guess)
+            if guess in secret_word:
+                correct_letters.append(guess)
+                print("Korrekt! Du har " + str(turns) + " gissnigar kvar. \n")
+                if(len(correct_letters) == len(secret_word)):
+                    isPlaying = False
+                    print("Bra jobbat du räddade gubben! Ordet var: " + secret_word.upper())
+            else:
+                turns -= 1
+                print("Fel! Du har " + str(turns) + " gissningar kvar. \n")
+        else: 
+            print("Du har redan gissat: " + guess)
     if turns == 0:
         isPlaying = False
-        print("Du förlorade! Ordet var: " + secret_word)
-    for letter in secret_word: 
-        if letter in guessed_letters:
+        print("Oops! Där hängdes en gubbe! Ordet var: " + secret_word.upper()) 
+
+    
+
+    # for letters in secret_word:   
+    #     if letter == guessed_letters:
+    #         isPlaying = False
+    #         print("Bra jobbat du klarade det! Ordet var: " + secret_word.upper())
+            
 
 # För nästa gång: kolla om bokstäverna i secret_word finns i guessed_letters om alla finns isPlaying = False         
         
-
 
